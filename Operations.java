b8dcbff21347d53d9fcb4139807b0b38f9c3a102
lssb123 <blikhitsrisai@gmail.com>
2023-08-23 20:27:21 +0530

    Implemented Subtraction Operations

---
+++ Operations.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Operations
+{
+	public static void operation(String operation, int a, int b){
+		System.out.println(operation);
+		System.out.println(a-b);
+		
+	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num1 = 1;
+		int num2 = 2;
+		String operation = "-";
+		operation(operation,num2,num1);
+	}
+}
